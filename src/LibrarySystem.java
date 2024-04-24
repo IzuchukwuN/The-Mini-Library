5d9a86ade4fc793d6824e2806cd1522bda539105
IzuchukwuN <kinglostvayne@gmail.com>
2024-04-23 22:06:32 -0400

    Card Number with comments commit

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class LibrarySystem {
     private String username;
     private String password;
@@ -20,8 +21,36 @@ public class LibrarySystem {
         this.password = password;
     }
 
+    // Method that creates a random 9-digit number
     public int cardNum(){
         System.out.print("Your library card number is: ");
         return (int)(Math.random()*(Integer.MAX_VALUE-1000000000))+1000000000;
     }
+    public static void libraryCardCreation() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Welcome to the Mini Library!");
+            System.out.println("1. Create a library card");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Library card created successfully!");
+                    break;
+                case 2:
+                    System.out.println("Exiting the Mini Library!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please enter 1 or 2.");
+            }
+        }
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
+        LibrarySystem.libraryCardCreation();
         LibrarySystem library = new LibrarySystem();
 
         // Set username and password using setters
@@ -9,6 +10,7 @@ public class Main {
         // Retrieve username and password using getters
         System.out.println("Username: " + library.getUsername());
         System.out.println("Password: " + library.getPassword());
+        // Calls the cardNum method using the library object
         System.out.println(library.cardNum());
     }
 }
