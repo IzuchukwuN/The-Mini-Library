a16e43a4a6ba1375239e032309e2d86f8728ffa8
Nimra Ejaz <nimraejaz78@yahoo.com>
2024-04-30 02:46:51 -0400

    Guide Class

---
+++ src/LibraryGuide.java
@@ -0,0 +1,37 @@
+public class LibraryGuide {
+    // Private variables to store information about a library guide
+    private String name;
+    private String email;
+    private String employeeId;
+    private String phoneNumber;
+
+    // Constructor to initialize the LibraryGuide object with provided values
+    public LibraryGuide(String name, String email, String employeeId, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.employeeId = employeeId;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LibraryGuide() {
+
+    }
+
+    // Using getters to retrieve information about the library guide
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,12 @@ public class Main {
             System.out.println("Password: " + library.getPassword());
             // Calls the cardNum method using the library object
             System.out.println(library.cardNum());
-        }
+
+            LibraryGuide guide = new LibraryGuide("User User", "user.user@example.com", "123456", "+12345678900");
+
+            System.out.println("Name: " + guide.getName());
+            System.out.println("Email: " + guide.getEmail());
+            System.out.println("Employee ID: " + guide.getEmployeeId());
+            System.out.println("Phone Number: " + guide.getPhoneNumber());
     }
+}
